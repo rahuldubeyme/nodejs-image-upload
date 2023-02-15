97e6fea3f2dffe0d3b852b17ffeeff9a68852c5c
Rahul <dubeyrahul890@gmail.com>
2023-02-15 22:00:49 +0530

    img update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const upload = multer({dest: __dirname + '/uploads/images'});
+console.log('upload-==>',upload);
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,7 @@ app.use(express.static('public'));
 
 app.post('/upload', upload.single('photo'), (req, res) => {
     if(req.file) {
+        console.log('ureq.file-==>',req.file);
         res.json(req.file);
     }
     else throw 'error';
